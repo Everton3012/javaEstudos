11157ef267a208237a0d7510e28e5feae6d77fde
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-01-11 00:54:22 -0300

    Adicionar construtores à classe Produto e atualizar testes em ProdutoTeste

--- exercicios/src/Classes_e_motodos/classe/ProdutoTeste.java
+++ exercicios/src/Classes_e_motodos/classe/ProdutoTeste.java
@@ -3,13 +3,13 @@ package Classes_e_motodos.classe;
 public class ProdutoTeste {
 
     public static void main(String[] args) {
-        Produto p1 = new Produto();
+        Produto p1 = new Produto("Notebook");
         p1.nome = "Notebook";
         p1.preco = 4356.89;
         p1.desconto = 0.25;
-        var p2 = new Produto();
+        var p2 = new Produto("Caneta Preta", 12.1, 0.29);
         p2.nome = "Caneta Preta";
-        p2.preco = 12.56;
+        //p2.preco = 12.56;
         p2.desconto = 0.29;
 
         double precoFinal1 = p1.precoComDesconto(.99);
