809db0f29f01b0f92a8737284750508bdbccc57e
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-08-14 16:55:26 -0300

    Adicionar classe Media com métodos para calcular e combinar médias, e implementar classe Reduce3 para demonstrar o uso de streams na manipulação de listas de alunos

--- exercicios/src/StreamAPI/MediaTeste.java
+++ exercicios/src/StreamAPI/MediaTeste.java
@@ -2,4 +2,17 @@ package StreamAPI;
 
 public class MediaTeste {
 
+    public static void main(String[] args) {
+        Media m1 = new Media().adicionar(8.3).adicionar(6.7).adicionar(9.8);
+
+        m1.adicionar(6.7);
+
+        Media m2 = new Media().adicionar(7.8).adicionar(5.4);
+
+        m2.adicionar(7.8);
+        System.out.println(m1.getValor());
+        System.out.println(m2.getValor());
+
+        System.out.println(Media.combinar(m1, m2).getValor());
+    }
 }
